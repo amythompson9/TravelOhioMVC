f0c5f61f2deea2acbedfb2447346ca202a0d783a
amythompson9 <amythompson9@gmail.com>
2017-10-22 14:43:16 -0400

    Drop Down NavBar

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace TravelOhioMVC
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Bootswatch.css",
                       "~/Content/site.css"));
         }
     }
